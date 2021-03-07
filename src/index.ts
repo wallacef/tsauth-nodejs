ce40c5e780baed9ea2465fc40748f1739ef79782
Wallace C. Ferrante <wallace.ferrante@monitoratec.com.br>
2021-03-07 10:48:25 -0300

    added the celebrate dependency for data validation on request

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import 'reflect-metadata';
 import express from 'express';
 import './database/connect';
 import routes from './routes';
+import cors from 'cors';
+import { errors } from 'celebrate';
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(errors());
 
 app.listen(process.env.PORT, () => console.log(`Server started at http://localhost:${process.env.PORT}`));
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 
 import authMiddleware from './app/middlewares/authMiddleware';
 
@@ -7,8 +8,26 @@ import AuthController from './app/controllers/AuthController';
 
 const router = Router();
 
-router.post('/users', UserController.store);
-router.post('/auth', AuthController.authenticate);
+router.post('/users', 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required().min(5).max(128),
+            email: Joi.string().required().email(),
+            password: Joi.string().required().min(6).max(8),
+        })
+    }), UserController.store);
+
+router.post('/auth',  
+    celebrate({
+        body: Joi.object().keys({
+            email: Joi.string().required().email(),
+            password: Joi.string().required().min(6).max(8),
+        })
+    }), AuthController.authenticate);
+
+/**
+ * Authenticate Routes
+ */
 router.get('/users', authMiddleware, UserController.index);
 
 export default router;
